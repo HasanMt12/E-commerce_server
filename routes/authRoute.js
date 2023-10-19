aa6601be2430a803701474b6b1527d187daa3f1c
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-10-19 16:56:34 +0600

    Implemented user registration and login using bcrypt for password hashing and JWT for authentication.

---
+++ routes/authRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import { registerController,
+    loginController,
+} from "../controller/authController.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+export default router;
